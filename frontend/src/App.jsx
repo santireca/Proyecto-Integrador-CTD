32847491cb5a12782bf2e4292d7c5815d42c7213
Alejandra <maayepesa@gmail.com>
2023-10-19 20:30:32 -0500

    Primer commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+    <div 
+      className=" flex flex-col justify-center items-center bg-red-300 mt-3 rounded-md w-[200px] p-2 
+      sm:bg-primary sm:w-[300px] hover:bg-green-300 transition duration-200 hover:scale-105 cursor-pointer"
+    >
+      <h1>
+        Proyecto Integrador
+      </h1>
+      <h2>
+        Instrumentos
+
+      </h2>
+
+    </div>
   )
 }
